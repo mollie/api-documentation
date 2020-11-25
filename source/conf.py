3e456ba6707bca83ca181f7bf87d1ed1cae64b38
Daniel Martinez Castillo <daniel.castillo@mollie.com>
2020-11-25 09:37:34 +0100

    MP-81 test without sptheme extension

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath("extensions"))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["mollie.setup", "cloud_sptheme.ext.table_styling"]
+extensions = ["mollie.setup"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
